e600c7d4f658763c067a41ea9091817722d64b61
TellexC <103390990+TellexC@users.noreply.github.com>
2022-04-15 19:56:03 -0700

    Update Parry Note.lua

--- custom_notetypes/Parry Note.lua
+++ custom_notetypes/Parry Note.lua
@@ -3,7 +3,7 @@ function onCreate()
 	for i = 0, getProperty('unspawnNotes.length')-1 do
 		--Check if the note is a blue note
 		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Parry Note' then
-			setPropertyFromGroup('unspawnNotes', i, 'texture', 'NOTE_parry'); --Change texture
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'Cuphead/NOTE_parry'); --Change texture
 
 			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
 				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', true); --Miss has penalties
@@ -13,19 +13,15 @@ function onCreate()
 	--debugPrint('Script started!')
 end
 
--- Function called when you hit a note (after note hit calculations)
--- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
--- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
--- noteType: The note type string/tag
--- isSustainNote: If it's a hold note, can be either true or false
 function noteMiss(id, noteData, noteType, isSustainNote)
 	if noteType == 'Parry Note' then
 		--donothinglmfao
 	end
 end
 
+canHitCup = false
+
 function goodNoteHit(id, noteData, noteType, isSustainNote)
 	if noteType == 'Parry Note' then
-		playSound('sounds/parry')
 	end
 end
